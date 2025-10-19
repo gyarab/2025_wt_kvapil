7a956ee0254d331414c40c05785239ef8e5d0451
Jakub Kvapil <jakub.kvapil.s@gyarb.cz>
2025-10-19 14:57:02 +0200

    some signin/up changes + new functionality

--- microsite/script.js
+++ microsite/script.js
@@ -1,8 +1,12 @@
 //newsletter sign-up button 
 const input = document.getElementById('newsletter');
-const signUpBtn = document.getElementById('signUpBtn');
+const newsletterBtn = document.getElementById('newsletter-btn');
 
-signUpBtn.addEventListener('click', () => {
+newsletterBtn.addEventListener('click', () => {
+    input.value = '';
+});
+
+signInBtn.addEventListener('click', () => {
     input.value = '';
 });
 
@@ -12,18 +16,39 @@ input.addEventListener("keydown", (event) => {
     }
 });
 
+
+
 //popup for signup/signim
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
 const popup = document.getElementById('signUp');
-const openBtn = document.getElementById('signUpBtn');
+const signUpBtn = document.getElementById('signUpBtn');
+const logInBt = document.getElementById('signInBtn');
 const closeBtn = document.getElementById('closeSignUp');
+const useBtn = document.getElementById('logIn');
+const nadpis = document.querySelector('uvod');
 
 //makes it hidden from start
 popup.style.display = 'none';
 
 signUpBtn.addEventListener('click', () => {
     popup.style.display = 'flex';
+    uvod.style.display = 'none';
+});
+
+signInBtn.addEventListener('click', () => {
+    popup.style.display = 'flex';
+        uvod.style.display = 'none';
+});
+
+useBtn.addEventListener('click', () => {
+    emailInput.value = '';
+    passwordInput.value = '';
+    popup.style.display = 'none';
+        uvod.style.display = 'block';
 });
 
 closeBtn.addEventListener('click', () => {
     popup.style.display = 'none';
+        uvod.style.display = 'block';
 });
