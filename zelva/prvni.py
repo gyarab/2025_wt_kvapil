40f45c87e1296b545a7430abf6bffc62e9a3154a
Jakub Kvapil <toustovac.cz@gmail.com>
2026-01-15 19:55:48 +0100

    made zelva draw Zeměkoule

--- zelva/prvni.py
+++ zelva/prvni.py
@@ -5,17 +5,23 @@ from math import sqrt
 def domecek(a):
     c = sqrt(2*(a**2))
     forward(a)
-    left(90)
+    left(135)
+    forward(c)
+    left(135)
     forward(a)
-    left(90)
+    left(135)
+    forward(c)
+    left(135)
     forward(a)
-    left(90)
-    forward(a)
-    left(90)
+    right(135)
+    forward(c/2)
+    right(90)
+    forward(c/2)
+    right(45)
     forward(a)
 
-for i in range(11):
-    domecek(random.randint(10, 20))
-    right(36)
+for i in range(7):
+    domecek(random.randint(10, 18))
+    left(36)
 
 exitonclick()
