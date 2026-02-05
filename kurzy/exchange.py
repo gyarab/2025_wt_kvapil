0934a7caf86560b24fee5e58fee9c28dc0415121
Jakub Kvapil <toustovac.cz@gmail.com>
2026-02-05 20:23:08 +0100

    made the curency converter work (not pretty)

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,57 @@
 import httpx
+from colorama import just_fix_windows_console, Fore, Back, Style
+just_fix_windows_console()
 
-r = httpx.get('URL z ukolu 2')
-print(r.text)
-print(r.status)
-lines = r.text.split('\n')
-print(lines[0])
+res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
+
+print("server odpověděl:", res.status_code)
+lines = res.text.split('\n')
+print(Back.BLUE + "Kruzy pro den:", Style.BRIGHT + lines[0].split(" ")[0] + Style.RESET_ALL)
+
+line_euro = ""
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+
+rate_eur_str = line_euro.split('|')[-1].replace(',' , '.')
+rate_eur = float(rate_eur_str)
+
+rate_czk = 1 / rate_eur
+
+print("Kurz eura je", Fore.GREEN + f"{rate_eur:.2f}" + "CZK" + Style.RESET_ALL)
+
+curency = input("Na jakou měnu chceš převést? (CZK/EUR)? ")
+
+if curency.upper() == "EUR":
+    value_in = input("Kolik máš eur? ")
+
+    try:
+        value_in = float(value_in)
+    except ValueError:
+        print("Lze zadat pouze číslo")
+        exit(1)
+
+    if (value_in < 0):
+        print("Nelze konvertovat záporné")
+        exit(1)
+
+    value_out = value_in * rate_eur
+
+    print(f" {value_in}€ je {value_out:.2f}CZK")
+elif curency.upper() == "CZK":
+    value_in = input("Kolik máš korun? ")
+
+    try:
+        value_in = float(value_in)
+    except ValueError:
+        print("Lze zadat pouze číslo")
+        exit(1)
+
+    if (value_in < 0):
+        print("Nelze konvertovat záporné")
+        exit(1)
+
+    value_out = value_in * rate_czk
+
+    print(f" {value_in}CZK je {value_out:.2f}€")
